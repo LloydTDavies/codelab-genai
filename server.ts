4b408177e6c0c63635326426d6f289c851d53afe
Lloyd Davies <lloydtdaviesit@gmail.com>
2024-09-24 18:12:51 +0000

    observe generated content

--- server.ts
+++ server.ts
@@ -12,74 +12,80 @@ import { GoogleAuth } from 'google-auth-library';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
-    const server = express();
-    const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-    const browserDistFolder = resolve(serverDistFolder, '../browser');
-    const indexHtml = join(serverDistFolder, 'index.server.html');
-    const commonEngine = new CommonEngine();
+  const server = express();
+  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+  const browserDistFolder = resolve(serverDistFolder, '../browser');
+  const indexHtml = join(serverDistFolder, 'index.server.html');
+  const commonEngine = new CommonEngine();
 
-    const auth = new GoogleAuth();
+  const auth = new GoogleAuth();
 
-    server.set('view engine', 'html');
-    server.set('views', browserDistFolder);
+  server.set('view engine', 'html');
+  server.set('views', browserDistFolder);
 
-    // Example Express Rest API endpoints
-    server.get('/api/facts', async (req, res) => {
-        const project = await auth.getProjectId();
+  // Example Express Rest API endpoints
+  server.get('/api/facts', async (req, res) => {
+    const project = await auth.getProjectId();
 
-        const vertex = new VertexAI({ project: project });
-        const generativeModel = vertex.getGenerativeModel({
-            model: 'gemini-1.5-flash',
-            generationConfig: { responseMimeType: 'application/json' }
-        });
+    const vertex = new VertexAI({ project: project });
+    const generativeModel = vertex.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+      generationConfig: { responseMimeType: 'application/json' }
+    });
 
-        const animal = req.query['animal'] || 'dog';
-        const prompt = `Give me 10 fun facts about ${animal}. 
+    const animal = req.query['animal'] || 'dog';
+    const prompt = `Give me 10 fun facts about ${animal}. 
                         Return as json as an array in the format ['fact 1', 'fact 2']
                         Remove backticks and other markdown formatting.`;
 
-        const resp = await generativeModel.generateContent(prompt);
-        let factArray = '';
-
-        if (resp.response.candidates) {
-            factArray = JSON.parse(resp.response.candidates[0].content.parts[0].text || '');
-        }
-        res.send(factArray);
-    });
-
-    // Serve static files from /browser
-    server.get('**', express.static(browserDistFolder, {
-        maxAge: '1y',
-        index: 'index.html',
+    const resp = await generativeModel.generateContent(prompt);
+    console.log(JSON.stringify({
+      severity: 'DEBUG',
+      message: 'Content is generated',
+      prompt: prompt,
+      response: resp.response,
     }));
+    let factArray = '';
 
-    // All regular routes use the Angular engine
-    server.get('**', (req, res, next) => {
-        const { protocol, originalUrl, baseUrl, headers } = req;
+    if (resp.response.candidates) {
+      factArray = JSON.parse(resp.response.candidates[0].content.parts[0].text || '');
+    }
+    res.send(factArray);
+  });
 
-        commonEngine
-            .render({
-                bootstrap,
-                documentFilePath: indexHtml,
-                url: `${protocol}://${headers.host}${originalUrl}`,
-                publicPath: browserDistFolder,
-                providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-            })
-            .then((html) => res.send(html))
-            .catch((err) => next(err));
-    });
+  // Serve static files from /browser
+  server.get('**', express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: 'index.html',
+  }));
 
-    return server;
+  // All regular routes use the Angular engine
+  server.get('**', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+
+    commonEngine
+      .render({
+        bootstrap,
+        documentFilePath: indexHtml,
+        url: `${protocol}://${headers.host}${originalUrl}`,
+        publicPath: browserDistFolder,
+        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+      })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
+  });
+
+  return server;
 }
 
 function run(): void {
-    const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 4000;
 
-    // Start up the Node server
-    const server = app();
-    server.listen(port, () => {
-        console.log(`Node Express server listening on http://localhost:${port}`);
-    });
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
 }
 
 run();
